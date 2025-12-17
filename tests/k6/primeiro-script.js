2256d1bf7f19f879c9cec22b7cc59cbc9d1bfb01
allexandresampaio <allexandresss@gmail.com>
2025-12-17 08:53:07 -0300

    primeiros testes e assertions com k6 no servidor local

--- tests/k6/primeiro-script.js
+++ tests/k6/primeiro-script.js
@@ -8,23 +8,69 @@ export const options = {
 
 };
 
-//o teste de fato
-export default function() {
-  let res = http.post(
-    'http://localhost:3000/instructors/login', 
+//os testes de fato
+export default function () {
+
+  let responseInstructorRegister = http.post(
+    'http://localhost:3000/instructors/register',
     JSON.stringify({
-        email: "al@le",
-        password: "123456"
+      name: "alle",
+      email: "al@le",
+      password: "123456"
     }),
     {
-        headers: {
-            'Content-Type': 'application/json'
-        }
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
   );
 
-  console.log(res.body)
-  
+  check(responseInstructorRegister, {
+    'status (create instructor) deve ser igual a 201': (resposta) => resposta.status === 201
+  });
 
+  //console.log(responseInstructorRegister.body)
+  sleep(1);
+
+  let responseInstructorLogin = http.post(
+    'http://localhost:3000/instructors/login',
+    JSON.stringify({
+      email: "al@le",
+      password: "123456"
+    }),
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  );
+
+  check(responseInstructorLogin, {
+    'status (login) deve ser igual a 200': (resposta) => resposta.status === 200
+  });
+
+  //console.log(responseInstructorLogin.json('token'))
+  let tokenInstructorLogin = responseInstructorLogin.json('token')
+  sleep(1);
+
+  let responseCreateLesson = http.post(
+    'http://localhost:3000/lessons',
+    JSON.stringify({
+      title: "IA para Testes",
+      description: "Aqui aprenderemos como usar IA para potencializar as tarefas de testes."
+    }),
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${tokenInstructorLogin}`
+      }
+    }
+  );
+  
+  //console.log(responseCreateLesson)
+
+  check(responseCreateLesson, {
+    'status (create lesson) deve ser igual a 201': (resposta) => resposta.status === 201
+  });
   sleep(1);
 }
